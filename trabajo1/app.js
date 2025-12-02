20873ca0fae6d2660ffbc1f26bcef5d1ffd6e031
falconsebas23-prog <salazares2005@gmail.com>
2025-12-02 16:11:40 -0500

    Se ha ajustado algunos comentarios de los puntos trabajados, en este caso se añadieron comentarios en el trabajo1

--- trabajo1/app.js
+++ trabajo1/app.js
@@ -4,9 +4,11 @@ const ingresar = leer();
 // Un agricultor tiene un terreno rectangular y conoce su base y su altura en metros. Quiere calcular tanto el área como el perímetro del terreno para cercarlo y sembrar. Diseña un programa en el que él pueda obtener ambos valores.
 
 // se crean y se suponen para practica de la variable
+// paralelograma (dfd)
 let base = parseFloat(ingresar("Ingrese la base del rectangulo: "));
 let altura = parseFloat(ingresar("Ingrese la altura del rectangulo: "));
 
+// rectangulo (dfd)
 let area=base*altura;
 // perimetro es para la operacion
 let perimetro = (2 * base) + (2 + altura);
